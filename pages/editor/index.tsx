80a03b92dc92383871c330c95dddcd291f897fd6
Bayu Oktari <bayu.oktari@tanihub.com>
2022-10-04 22:38:01 +0700

    [MODIFY] move slate editor to editor page [ADD] new home page

---
+++ pages/editor/index.tsx
@@ -0,0 +1,83 @@
+import React, { useState, useMemo, useCallback } from 'react'
+import { Box, Paper } from '@mui/material'
+import { createEditor, Descendant } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+import Head from 'next/head'
+import styled from 'styled-components'
+
+import DefaultElement from '../../components/DefaultElement'
+import Leaf from '../../components/Leaf'
+import TitleElement from '../../components/TitleElement'
+import ToolBar from '../../components/ToolBar'
+
+import type { RenderElementProps } from 'slate-react'
+import type { NextPage } from 'next'
+
+const StyledBox = styled(Box)`
+  padding: 100px 130px;
+  width: 100%;
+  height: 100vh;
+  background-color: #dfe0e1;
+  overflow-y: scroll;
+`
+
+const StyledContentBox = styled(Box)`
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+const initialValue: Descendant[] = [
+  {
+    type: 'title',
+    children: [{ text: 'Get Ready to Type Your First Article !' }],
+  },
+  {
+    type: 'paragraph',
+    children: [
+      {
+        text: 'Type whatever you like',
+      },
+    ],
+  },
+]
+
+const Editor: NextPage = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+  const [value, setValue] = useState(initialValue)
+
+  const renderElement = useCallback((props: RenderElementProps) => {
+    switch (props.element.type) {
+      case 'title':
+        return <TitleElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Rich Text Editor</title>
+      </Head>
+      <StyledBox>
+        <Paper elevation={4}>
+          <StyledContentBox p={5} height="100vh">
+            <Slate editor={editor} value={value} onChange={setValue}>
+              <ToolBar />
+              <Editable
+                renderElement={renderElement}
+                renderLeaf={(props) => <Leaf {...props} />}
+                autoFocus
+              />
+            </Slate>
+          </StyledContentBox>
+        </Paper>
+      </StyledBox>
+    </>
+  )
+}
+
+export default Editor
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,82 +1,63 @@
-import React, { useState, useMemo, useCallback } from 'react'
-import { Box, Paper } from '@mui/material'
-import { createEditor, Descendant } from 'slate'
-import { Slate, Editable, withReact } from 'slate-react'
-import Head from 'next/head'
+import React from 'react'
+import { Box, Typography, Button, Grid } from '@mui/material'
+import { useRouter } from 'next/router'
 import styled from 'styled-components'
 
-import DefaultElement from '../components/DefaultElement'
-import Leaf from '../components/Leaf'
-import TitleElement from '../components/TitleElement'
-import ToolBar from '../components/ToolBar'
-
-import type { RenderElementProps } from 'slate-react'
 import type { NextPage } from 'next'
 
-const StyledBox = styled(Box)`
-  padding: 100px 130px;
-  width: 100%;
-  height: 100vh;
-  background-color: #dfe0e1;
-  overflow-y: scroll;
-`
-
-const StyledContentBox = styled(Box)`
-  overflow-y: scroll;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  ::-webkit-scrollbar {
-    display: none;
+const StyeldBox = styled(Box)`
+  background: linear-gradient(244deg, #c29d68, #84f0e2, #c586b8, #b59323);
+  background-size: 240% 240%;
+  animation: gradient-animation 40s ease infinite;
+  @keyframes gradient-animation {
+    0% {
+      background-position: 0% 50%;
+    }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
   }
 `
-const initialValue: Descendant[] = [
-  {
-    type: 'title',
-    children: [{ text: 'Get Ready to Type Your First Article !' }],
-  },
-  {
-    type: 'paragraph',
-    children: [
-      {
-        text: 'Type whatever you like',
-      },
-    ],
-  },
-]
 
 const Home: NextPage = () => {
-  const editor = useMemo(() => withReact(createEditor()), [])
-  const [value, setValue] = useState(initialValue)
-
-  const renderElement = useCallback((props: RenderElementProps) => {
-    switch (props.element.type) {
-      case 'title':
-        return <TitleElement {...props} />
-      default:
-        return <DefaultElement {...props} />
-    }
-  }, [])
-
+  const router = useRouter()
   return (
-    <>
-      <Head>
-        <title>Rich Text Editor</title>
-      </Head>
-      <StyledBox>
-        <Paper elevation={4}>
-          <StyledContentBox p={5} height="100vh">
-            <Slate editor={editor} value={value} onChange={setValue}>
-              <ToolBar />
-              <Editable
-                renderElement={renderElement}
-                renderLeaf={(props) => <Leaf {...props} />}
-                autoFocus
-              />
-            </Slate>
-          </StyledContentBox>
-        </Paper>
-      </StyledBox>
-    </>
+    <StyeldBox
+      width="100vw"
+      height="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box>
+        <Typography variant="h2">Typedream Simple Rich Text Editor</Typography>
+        <Box mt={2}>
+          <Grid container spacing={4}>
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="inherit"
+                onClick={() => router.push('/editor')}
+              >
+                Try Text Editor
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant="outlined"
+                color="inherit"
+                href="https://linkedin.com/in/bayuoktari"
+              >
+                About Me
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </StyeldBox>
   )
 }
 
